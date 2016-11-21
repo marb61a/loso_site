47a92cec755aba3dafe18e9f4c08e610f4f8fbcf
marb <marb61a@gmail.com>
2016-11-21 10:42:28 +0000

    updated custom.js

--- js/custom.js
+++ js/custom.js
@@ -30,4 +30,32 @@ $(document).ready(function(){
             });
         }
     });
-})
+});
+
+// Add smooth scrolling
+$(document).ready(function(){
+    'use strict';
+    $('.nav-item, #scroll-to-top').click(function(){
+        if(location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname){
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if(target.length){
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                
+                return false;
+            }
+        }
+    });
+});
+
+// Active menu item on click
+$(document).ready(function() {
+    'use strict';
+    $('.navbar-nav li a').click(function(){
+        'use strict';
+        $('.navbar-nav li a').parent().removeClass("active");
+        $(this).parent().addClass("active");
+    });
+});
